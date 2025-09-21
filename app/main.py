ffdfcaa138ddf09661a8f52a119dac608ec44cb6
Kelsit0 <ramosreynagaoscar6@gmail.com>
2025-09-21 13:37:26 -0600

    add cors

--- app/main.py
+++ app/main.py
@@ -3,9 +3,20 @@ from typing import Union
 from fastapi import FastAPI
 from app.api import analyze
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 app = FastAPI()
 
 app.include_router(analyze.router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # tu app Angular
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def read_root():
